656adcf1ce562168b7d883a13336eb9d63b3658d
migsamu <mufrikiler@gmail.com>
2021-12-13 14:14:12 +0100

    list and post customer hecho

--- src/main/java/org/iesfm/bank/ccontroller/CustomerController.java
+++ src/main/java/org/iesfm/bank/ccontroller/CustomerController.java
@@ -1,4 +1,28 @@
 package org.iesfm.bank.ccontroller;
 
+import org.iesfm.bank.pojos.Customer;
+import org.iesfm.bank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/customers")
+    public List<Customer> list() {
+        return customerRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/customers")
+    public void insert(@RequestBody Customer customer) {
+        customerRepository.save(customer);
+    }
 }
