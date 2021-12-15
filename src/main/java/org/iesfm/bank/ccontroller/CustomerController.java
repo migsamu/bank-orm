feaec2ff12c0262d7e9934a33fefa7e8efa2e5c1
migsamu <mufrikiler@gmail.com>
2021-12-15 14:13:15 +0100

    customer acabado

--- src/main/java/org/iesfm/bank/ccontroller/CustomerController.java
+++ src/main/java/org/iesfm/bank/ccontroller/CustomerController.java
@@ -3,12 +3,12 @@ package org.iesfm.bank.ccontroller;
 import org.iesfm.bank.pojos.Customer;
 import org.iesfm.bank.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CustomerController {
@@ -23,6 +23,28 @@ public class CustomerController {
 
     @RequestMapping(method = RequestMethod.POST, path = "/customers")
     public void insert(@RequestBody Customer customer) {
-        customerRepository.save(customer);
+        if (customerRepository.existsByNif(customer.getNif())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "El cliente ya existe");
+        } else {
+            customerRepository.save(customer);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/customers/{id}")
+    public void delete(@PathVariable int id) {
+        if (!customerRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente no encontrado");
+        } else {
+            customerRepository.deleteById(id);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/customers/{id}")
+    public Optional<Customer> listById(@PathVariable int id) {
+        if (!customerRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente no encontrado");
+        } else {
+            return customerRepository.findById(id);
+        }
     }
 }
--- src/main/java/org/iesfm/bank/repository/CustomerRepository.java
+++ src/main/java/org/iesfm/bank/repository/CustomerRepository.java
@@ -4,4 +4,5 @@ import org.iesfm.bank.pojos.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    boolean existsByNif(String nif);
 }
